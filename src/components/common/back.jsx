482935e2487be8fcf8f111609a60c4546677d7fc
Martin <martinjesu.joseph@gmail.com>
2021-02-08 09:15:01 +0530

    Back header changes

--- src/components/common/back.jsx
+++ src/components/common/back.jsx
@@ -4,7 +4,19 @@ export default function Back(props) {
 
     return (
         <div id="back-container" className="">
-            
+            <div className="back-container-content" >
+                <i className="arrow left ml-2"></i>
+                <span className="back-header">Back to Reward Zone</span>
+                <div class="notification-box">
+                    <span class="notification-count">6</span>
+                    <div class="notification-bell">
+                        <span class="bell-top"></span>
+                        <span class="bell-middle"></span>
+                        <span class="bell-bottom"></span>
+                        <span class="bell-rad"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
